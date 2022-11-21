5b4446a9e7302bfea869aebdacc5d244a0eed157
N a n d a <101814517+AnandaAfriezta@users.noreply.github.com>
2022-11-21 08:07:45 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" 
+href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/litera/bootstrap.min.css" 
+integrity="sha384-enpDwFISL6M3ZGZ50Tjo8m65q06uLVnyvkFO3rsoW0UC15ATBFz3QEhr3hmxpYsn" 
+crossorigin="anonymous">
+<style>
+  p {text-align: center;}
+  q {}
+</style>
+    <title></title>
+</head>
+<body>
+    <div class="row" style="margin-top:50px;">
+    <div class="col-md"></div>
+    <div class="col-md rounded bg-light" style="box-shadow: 0px 0px 15px 1000px;padding:50px">
+        <form action="proses_login.php" method="post">
+        <p><a href="https://imgbb.com/"><img src="https://i.ibb.co/nR10GDP/KULIN-REMOVE-1.png" alt="KULIN-REMOVE-1" border="0"></a></p>
+          <h1 align="center">LOG IN</h1>
+          <br>
+          <br>
+          <h5 style="color: rebeccapurple; font-family:montserrat; font-size: 18px;">Username :</h5>
+          <input type="text" name="username" value="" class="form-control">
+          <br>
+          <h5 style="color: rebeccapurple; font-family:montserrat; font-size: 18px;">Password :</h5>
+          <input type="password" name="password" class="form-control"><br>
+          <br>
+          <center><input type="submit" name="login" class="btn btn-success" value="LOGIN"></center>
+      </form>
+    </div>
+    <div class="col-md"></div>
+  </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" 
+    crossorigin="anonymous"></script>
+</body>
+</html>
+ 
